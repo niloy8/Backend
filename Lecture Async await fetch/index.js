6742c7c1a4163982ead350ead7d0fd3b46116ef1
niloy8 <niloybhuiyan688@gmail.com>
2025-06-16 07:50:00 +0600

    post something

--- Lecture Async await fetch/index.js
+++ Lecture Async await fetch/index.js
@@ -13,3 +13,27 @@ const Data = async () => {
     console.log(data)
 }
 Data()
+
+
+/**Post request */
+const datas = {
+    title: 'foo',
+    email: 'hello@gmail.com'
+}
+
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(datas)
+}
+
+const fun = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+    const data = await response.json()
+    console.log(data)
+}
+
+fun()
