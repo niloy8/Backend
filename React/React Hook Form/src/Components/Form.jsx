f0a76d6ab899631b165dec1eb30e0c9d83194ab3
niloy8 <niloybhuiyan688@gmail.com>
2025-06-22 11:28:19 +0600

    Submitting added

--- React/React Hook Form/src/Components/Form.jsx
+++ React/React Hook Form/src/Components/Form.jsx
@@ -1,19 +1,34 @@
 import { useForm } from "react-hook-form"
 
 const Form = () => {
-    const { register, handleSubmit } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm();
 
 
-    const onSubmit = (d) => {
+
+    const onSubmit = async (d) => {
+
+        // Simulate API call delay
+        await new Promise((resolve) => setTimeout(resolve, 2000));
         console.log(d)
-    }
+        console.log("Submission finished");
+    };
+
 
     return (
         <div>
             <form action="#" onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" {...register("firstName")} />
+                <input type="text" {...register("firstName")} required />
+                {errors.firstName && <span>First name is required</span>}
                 <input type="text" {...register("lastName")} />
-                <input type="submit" />
+                <input
+                    type="submit"
+                    value={isSubmitting ? "Submitting..." : "Submit"}
+                    disabled={isSubmitting}
+                />
             </form>
         </div>
     );
