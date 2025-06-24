09e2c9476938c7ec616057758267fc4cf9be00df
niloy8 <niloybhuiyan688@gmail.com>
2025-06-24 19:50:14 +0600

    POST method added

--- user-management-server/index.js
+++ user-management-server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 
 app.use(cors())
+app.use(express.json())
 const users = [
     {
         id: 1,
@@ -33,6 +34,15 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.send(users)
+})
+
+app.post("/users", (req, res) => {
+    console.log('post api hitting')
+    console.log(req.body)
+    const newUser = req.body
+    newUser.id = users.length
+    users.push(newUser)
+    res.send(newUser)
 })
 
 app.listen(port, () => {
