947ca7b3cd96317a8b25a5d72e7e17d7464b794a
niloy8 <niloybhuiyan688@gmail.com>
2025-06-29 09:07:15 +0600

    POST api created and posted data

--- coffe-client/src/Components/Signin.jsx
+++ coffe-client/src/Components/Signin.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/Provider";
+import Swal from "sweetalert2";
 
 const Signin = () => {
     const { createUser } = useContext(AuthContext)
@@ -7,10 +8,32 @@ const Signin = () => {
         e.preventDefault()
         console.log(e)
         const email = e.target.email.value
+        const name = e.target.name.value
         const password = e.target.password.value
+        const NewUser = { name, email }
         createUser(email, password)
             .then(result => {
                 console.log(result)
+                fetch('http://localhost:3000/users', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(NewUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.insertedId) {
+                            Swal.fire({
+                                title: 'Success',
+                                text: 'Added Successfully',
+                                icon: 'success',
+                                confirmButtonText: 'Cool'
+                            })
+                            e.target.reset()
+                        }
+                    })
             })
             .catch(err => { console.log(err) })
     };
@@ -25,6 +48,8 @@ const Signin = () => {
                         <div className="card-body">
                             <form onSubmit={handleSignup}>
                                 <fieldset className="fieldset">
+                                    <label className="label">Name</label>
+                                    <input type="text" className="input" placeholder="name" name="name" />
                                     <label className="label">Email</label>
                                     <input type="email" className="input" placeholder="Email" name="email" />
                                     <label className="label">Password</label>
--- coffe-server/index.js
+++ coffe-server/index.js
@@ -59,6 +59,15 @@ async function run() {
             const result = await coffeCollection.findOne(query)
             res.send(result)
         })
+
+
+        //user DB
+        const userCollection = client.db('coffeDB').collection('users')
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
